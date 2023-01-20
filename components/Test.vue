c967e495e0a07a54fe82638dfff0f5d9311478b1
Yaël Guilloux <yael.guilloux@gmail.com>
2023-01-20 16:11:44 +0100

    feat(up): up

---
+++ components/Test.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    <img :src="useAppConfig().docus.image" />
+  </div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,11 @@ export default defineNuxtConfig({
   extends: ['@nuxt-themes/docus'],
   studio: {
     enabled: true
-  }
+  },
+  components: [
+    {
+      path: '~/components',
+      global: true
+    }
+  ]
 })
