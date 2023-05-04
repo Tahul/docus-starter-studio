be3d4354af31de72f50821d142c94d46da209065
Yaël Guilloux <yael.guilloux@gmail.com>
2023-05-04 16:20:20 +0200

    chore(up): up
    up

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,7 +1,18 @@
-import { defineTheme, palette } from 'pinceau'
+import { defineTheme } from 'pinceau'
 
 export default defineTheme({
   color: {
-    primary: palette('orange')
+    primary: {
+      50: '#F2FAFF',
+      100: '#DFF3FF',
+      200: '#C6EAFF',
+      300: '#A1DDFF',
+      400: '#64C7FF',
+      500: '#1AADFF',
+      600: '#0069A6',
+      700: '#014267',
+      800: '#002235',
+      900: '#00131D'
+    }
   }
 })
